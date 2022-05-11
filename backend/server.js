95312c45d3156a608cbd7e89fc6863744d82a03f
taesik <taesic11@naver.com>
2022-05-11 15:31:32 +0900

    CORS test

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,24 @@
 const express = require("express");
-
+const cors = require('cors');
 const app = express();
+let allowed = ['http://localhost:3000','some other link'];
+// function options(req, res) {
+//   let tmp;
+//   let origin = req.header('Origin');
+//   if(allowed.indexOf(origin) >= 0) {
+//     tmp = {
+//       origin: true,
+//       optionSuccessStatus: 200,
+//     };
+//   }else {
+//     tmp = {
+//       origin: 'stupid',
+//     }
+//   }
+//   res(null,tmp);
+// }
+app.use(cors());
+
 
 app.get("/", (req, res) => {
   res.send("welcome from home");
