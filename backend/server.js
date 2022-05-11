3ca63ef00bf54d5523085b8e85cfa70c22788dc2
taesik <taesic11@naver.com>
2022-05-11 16:27:43 +0900

    Add environment

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
-const cors = require('cors');
-const {readdirSync} = require('fs');
-const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 
+const { readdirSync } = require("fs");
+const app = express();
 app.use(cors());
 
+readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
 
-readdirSync('./routes').map((r)=> app.use("/",require('./routes/'+r)));
-
-app.listen(8000, () => {
-  console.log("server is listening...");
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}..`);
 });
